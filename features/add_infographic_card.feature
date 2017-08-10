ff93aff14b4c6e020073be4a836e94c111f46abe
gsinghsdrds <gsingh@esri.com>
2017-08-09 18:01:13 -0700

    added new test case on infographics card

---
+++ features/add_infographic_card.feature
@@ -0,0 +1,7 @@
+Feature: Add Infographics USA Total Population Card
+
+  Scenario: Customer will add usa total population card
+    Given Customer goes to the Analytics menu
+    When  Customer selects Infographics from the Analytics menu
+    And   Customer selects the total population inforgraphics card
+    Then  Customer will see total population infographics card will appear
---
+++ features/steps/steps_infographics_card_total_population.py
@@ -0,0 +1,30 @@
+from behave import *
+from commonfiles.utility import UtilityHelperClass
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+use_step_matcher("re")
+
+
+@given("Customer goes to the Analytics menu")
+def step_i_select_analytics_toolbar(context):
+    ActionChains(context.browser).move_to_element(context.browser.find_element_by_xpath(".//*[starts-with(@id, 'uniqName')][2]/div/span/span[1]")).click().perform()
+
+@when("Customer selects Infographics from the Analytics menu")
+def step_driv_time_from_analytics_menu(context):
+    context.browser.find_element_by_xpath("//*[contains(text(),'Infographics')]").click()
+    UtilityHelperClass(context.browser).selenium_wait_interval(1)
+
+
+@when("Customer selects the total population inforgraphics card")
+def step_search_for_text_pizza_place_in_usa(context):
+    #ActionChains(context.browser).move_to_element(context.browser.find_element_by_xpath("//div[@class='demographicsPanel']/div[1]/div[2]/div[1]/div/input")).click().perform()
+    element = context.browser.find_element_by_xpath("//div[@class='demographicsPanel']/div[1]/div[2]/div[1]/div/input")
+    context.browser.execute_script("return arguments[0].click();", element)
+    UtilityHelperClass(context.browser).wait_interval(1)
+
+
+@then("Customer will see total population infographics card will appear")
+def step_pin_display_map_area(context):
+    if ActionChains(context.browser).move_to_element(context.browser.find_element_by_xpath("//div[@class='infographicsVariableGroup']/table/tbody/tr/td[1]/div[1]/span")) is True:
+        print ("USA Total Population Card is Visible ")
+    UtilityHelperClass(context.browser).wait_interval(1)
